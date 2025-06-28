84793d12977c1f9aa8e07f0f0e2fc45708ccd88a
Massa anthony <elio007@hotmail.fr>
2025-06-28 07:06:30 +0200

    add helmet

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
 var dotenv = require('dotenv');
+var helmet = require("helmet");
 
 dotenv.config();
 var usersRouter = require('./routes/users');
@@ -12,9 +13,11 @@ const {connectDatabase} = require('./Connect');
 var app = express();
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL, // Replace with your React app's URL 
-methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+  origin: process.env.FRONTEND_URL, 
+methods: ['GET', 'POST', 'PUT', 'DELETE'], 
 }));
+
+
 
 connectDatabase();
 
@@ -27,6 +30,15 @@ app.use('/images', express.static(path.join(__dirname, 'public/images')));
 // ...existing code...
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        "script-src": ["'self'"],
+      },
+    },
+  }),
+);
 app.use('/users', usersRouter);
 app.use('/product', productuser)
 
